8ea18c9ddb66840b2b4081e27fea27b2eebd4524
varun <varunvenugopaltr@gmail.com>
2021-09-25 17:31:50 +0530

    V1

---
+++ todo.js
@@ -0,0 +1,92 @@
+//redirect if not valid login
+let currentUser=JSON.parse(window.localStorage.getItem('user'));
+if(!currentUser){
+    window.location='index.html';
+
+}
+
+$('#welcomeText').html(`Hello ${currentUser.uname}! <br> Welcome to your Todo List`);
+
+// Logout Section
+$('#logOut').on('click',(e)=>{
+    e.preventDefault();
+    // Sign-out successful.
+    window.localStorage.removeItem('user');
+    window.location="index.html";
+    console.log('logout successfully');
+})
+
+// Data fetch
+const getList=async ()=>{
+    try{
+        const res=await axios.get('./todo.json');
+        const lists=res.data;
+        console.log(lists);
+        let listcontent='';
+        lists.forEach((el,index)=>{
+            listcontent+=`<li class="list-group-item ${el.completed?'disabledList':''} ${index%2?'list-group-item-info':'list-group-item-success'}"> <input type="checkbox" class="checkbox" ${el.completed?' checked':''}/> <label for=""> ${el.title}</label></li>`
+        });
+        $('#todoList').html(listcontent);
+        if(checkedCount){
+            checkedCount=0;
+        }
+
+    }
+    catch(e){
+        console.log('failed to fetch lists data',e);
+    }
+}
+// call getlist() when GET LIST is clicked.
+$('#getList').on('click',(e)=>{
+    e.preventDefault();
+    getList();
+});
+
+//variable to keep track of cheking list items
+let checkedCount=0;
+
+const alertPromise= ()=>{
+     return new Promise((resolve,reject)=>{
+
+         
+        if(checkedCount===5){
+            resolve(checkedCount)
+        }
+        else{
+            reject('count not equal to 5');
+        }
+    });
+}
+
+const promiseCall=()=>{
+    alertPromise().then((data)=>{
+        alert(`Kudos, We done ${data} activities today.... Congrats!`);
+    })
+    .catch((err)=>{
+        console.log('promise rejected');
+    })
+}
+
+
+getList();
+
+$('#todoList').on('change','.checkbox',function(e){
+    if($(this).prop('checked')===true){
+        console.log('checked');
+        checkedCount++; 
+        $(this).parent().addClass('active');
+    }
+    else{
+        checkedCount--;
+        console.log('unchecked');
+        $(this).parent().removeClass('active');
+    }
+    
+    promiseCall();
+
+
+});
+
+
+
+
